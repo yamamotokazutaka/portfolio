55d4e7fbad02c8db6e363025a2ac6264bb28ea4e
yamamotokazutaka <owner@MA4-0014.local>
2019-06-09 14:48:10 +0900

    Portfolio-first-commit

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,18 @@
+class AboutController < ApplicationController
+  def index
+
+  end
+  def profile
+
+  end
+  def works
+
+  end
+
+  def contact
+
+  end
+  def others
+    
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'about#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/about/profile" => "about#profile"
+  get "/about/works" => "about#works"
+  get "/about/contact" => "about#contact"
+  get "/about/others" => "about#others"
+end
