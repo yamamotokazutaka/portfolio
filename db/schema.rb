4d59dc182f092019eaceefa601ff96fa94575ae8
yamamotokazutaka <49389307+yamamotokazutaka@users.noreply.github.com>
2019-06-22 15:17:04 +0900

    Update gemfile.lock for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_073524) do
-
-  create_table "inquiries", force: :cascade do |t|
-    t.string "name"
-    t.string "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
