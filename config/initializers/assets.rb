bf60c5882937e0baac09a456019751e0802654c1
yamamotokazutaka <49389307+yamamotokazutaka@users.noreply.github.com>
2019-07-21 17:51:22 +0900

    scss

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,5 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += [
   "js/lightbox.js",
-  "css/lightbox.css"
+  "css/lightbox.scss"
 ]
